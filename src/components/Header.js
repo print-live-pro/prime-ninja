8ec0c20aa35db67baa6c2e81df2b98905a9e47a1
mac <mac@macs-MacBook-Pro.local>
2022-10-07 14:25:38 +0530

    Nadeem: tailwind css setpup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "tailwindcss/tailwind.css"
 
 const Header = () => {
-  return <div>Header</div>;
+  return <div className="text-red-700 !m-0 bg-yellow-50 mt-10 text-3xl font-bold underline">Header</div>;
 };
 
 export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+    './src/**/*.{js,jsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
