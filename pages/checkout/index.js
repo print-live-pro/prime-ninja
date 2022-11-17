25a6b5757330c584b6504484ee5c726a1df79f87
mac <mac@macs-MacBook-Pro.local>
2022-11-17 23:47:35 +0530

    changes

--- pages/checkout/index.js
+++ pages/checkout/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Head from "next/head"
-import Checkout from "../../src/components/checkout/checkout"
+import Checkout from "../../src/components/checkout/Checkout"
 
 const index = () => {
   return (
