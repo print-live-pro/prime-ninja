82c36c80449b1e143762061c32e2276e24291fe0
mac <mac@macs-MacBook-Pro.local>
2022-11-19 00:28:13 +0530

    nadem:added contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,14 +1,9 @@
-import React, { useEffect } from 'react'
+import React, { useEffect , useState } from 'react'
 import Header from "../src/components/Header";
 import Footer from "../src/components/Footer";
 import Head from "next/head"
+import Contact from '../src/components/contact/Contact';
 const contact = () => {
-    var email,subject,body;
-    useEffect(()=>{
-        email = document.getElementById("email").value;
-        subject = document.getElementById("subject").value;
-        body = document.getElementById("body");
-    },[])
   return (
     <>
     <Head>
@@ -21,27 +16,7 @@ const contact = () => {
         ></script>
       </Head>
       <Header />
-    <section class="bg-[#C7DBE6] dark:bg-gray-900">
-  <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-      <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
-      <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a bug? Let us know.</p>
-      <form action="#" class="space-y-8">
-          <div>
-              <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-              <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required/>
-          </div>
-          <div>
-              <label for="subject" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
-              <input type="text" id="subject" class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let us know how we can help you" required/>
-          </div>
-          <div class="sm:col-span-2">
-              <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-              <textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
-          </div>
-          <a href={`mailto:${email}?subject=${subject}&body=${body}`} target="_blank" rel="noopener noreferrer" type="submit" class="py-3 px-5 text-sm font-medium text-center text-black border border-black rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Email Us</a>
-      </form>
-  </div>
-</section>
+        <Contact/>
 <Footer />
 </>
   )
